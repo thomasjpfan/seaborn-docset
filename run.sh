e85205f4dcaa10a7716e55dbb0b919cdfac2be2b
Thomas Fan <thomasjpfan@gmail.com>
2018-07-31 16:17:54 -0400

    BLD: Fix

--- run.sh
+++ run.sh
@@ -3,19 +3,8 @@
 git submodule init
 git submodule update
 python -m ipykernel install --name seaborn-dev
-
-pushd seaborn
-pip install -e .
-popd
-
-pushd seaborn/doc/tutorial
-make
-popd
-
-pushd seaborn/doc
-make introduction
-make html
-popd
+make -C seaborn/doc notebooks
+make -C seaborn/doc html
 
 doc2dash -n Seaborn seaborn/doc/_build/html
 python add_samples_tutorial.py
